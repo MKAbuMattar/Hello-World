b9e6062c5d71a7edc5159dd62a1c62cf7f8b674a
MKAbuMattar <mohammad.khaled@outlook.com>
2021-04-10 16:33:43 +0300

    hotfix

--- assets/js/app.js
+++ assets/js/app.js
@@ -304,16 +304,16 @@ fetch(`./assets/data/all.json`)
 
   })
 
-// if (`serviceWorker` in navigator) {
-//   window.addEventListener(`load`, () => {
-//     navigator.serviceWorker.register(`/Hello-World/sw.js`).then((registration) => {
-//       console.log(`ServiceWorker registration successful with scope: `, registration
-//         .scope)
-//     }, function (err) {
-//       console.log(`ServiceWorker registration failed: `, err)
-//     })
-//   })
-// }
+if (`serviceWorker` in navigator) {
+  window.addEventListener(`load`, () => {
+    navigator.serviceWorker.register(`/Hello-World/sw.js`).then((registration) => {
+      console.log(`ServiceWorker registration successful with scope: `, registration
+        .scope)
+    }, function (err) {
+      console.log(`ServiceWorker registration failed: `, err)
+    })
+  })
+}
 
 mybutton = document.getElementById(`myBtn`)
 
--- sw.js
+++ sw.js
@@ -1,50 +1,50 @@
-// cacheName = `hello-word-v3`
-// staticAssets = [
-//     `./`,
-//     `./index.html`,
-//     `./assets/css/a11y-dark.css`,
-//     `./assets/css/normalize.css`,
-//     `./assets/css/style.css`,
-//     `./assets/js/app.js`,
-//     `./assets/data/all.json`,
-//     `./manifest.webmanifest`
-// ]
+cacheName = `hello-word-v3`
+staticAssets = [
+    `./`,
+    `./index.html`,
+    `./assets/css/a11y-dark.css`,
+    `./assets/css/normalize.css`,
+    `./assets/css/style.css`,
+    `./assets/js/app.js`,
+    `./assets/data/all.json`,
+    `./manifest.webmanifest`
+]
 
-// self.addEventListener(`install`, async e => {
-//     cache = await caches.open(cacheName)
-//     await cache.addAll(staticAssets)
-//     return self.skipWaiting()
-// })
+self.addEventListener(`install`, async e => {
+    cache = await caches.open(cacheName)
+    await cache.addAll(staticAssets)
+    return self.skipWaiting()
+})
 
-// self.addEventListener(`activate`, e => {
-//     self.clients.claim()
-// })
+self.addEventListener(`activate`, e => {
+    self.clients.claim()
+})
 
-// self.addEventListener(`fetch`, async e => {
-//     req = e.request
-//     url = new URL(req.url)
+self.addEventListener(`fetch`, async e => {
+    req = e.request
+    url = new URL(req.url)
 
-//     if (url.origin === location.origin) {
-//         e.respondWith(cacheFirst(req))
-//     } else {
-//         e.respondWith(networkAndCache(req))
-//     }
-// })
+    if (url.origin === location.origin) {
+        e.respondWith(cacheFirst(req))
+    } else {
+        e.respondWith(networkAndCache(req))
+    }
+})
 
-// cacheFirst = async (req) => {
-//     cache = await caches.open(cacheName)
-//     cached = await cache.match(req)
-//     return cached || fetch(req)
-// }
+cacheFirst = async (req) => {
+    cache = await caches.open(cacheName)
+    cached = await cache.match(req)
+    return cached || fetch(req)
+}
 
-// networkAndCache = async (req) => {
-//     cache = await caches.open(cacheName)
-//     try {
-//         fresh = await fetch(req)
-//         await cache.put(req, fresh.clone())
-//         return fresh
-//     } catch (e) {
-//         cached = await cache.match(req)
-//         return cached
-//     }
-// }
+networkAndCache = async (req) => {
+    cache = await caches.open(cacheName)
+    try {
+        fresh = await fetch(req)
+        await cache.put(req, fresh.clone())
+        return fresh
+    } catch (e) {
+        cached = await cache.match(req)
+        return cached
+    }
+}
